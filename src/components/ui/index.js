d70a99f58fe4458c9eb6a46cf226cc3fdd44dbaa
Shist <shist.pupust@mail.ru>
2025-01-06 15:03:39 +0100

    Added loading spinner while fetching coaches list from DB

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,9 +1,11 @@
 import BaseBadge from '@/components/ui//BaseBadge.vue';
 import BaseButton from '@/components/ui//BaseButton.vue';
 import BaseCard from '@/components/ui//BaseCard.vue';
+import BaseSpinner from '@/components/ui//BaseSpinner.vue';
 
 export default {
   BaseBadge,
   BaseButton,
   BaseCard,
+  BaseSpinner,
 };
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -4,11 +4,14 @@
     <BaseCard>
       <div class="controls">
         <BaseButton mode="outline" @click="loadCoaches">Refresh</BaseButton>
-        <BaseButton v-if="!isCoach" link to="/register">
+        <BaseButton v-if="!isCoach && !isLoading" link to="/register">
           Register as Coach
         </BaseButton>
       </div>
-      <ul v-if="hasCoaches">
+      <div v-if="isLoading">
+        <BaseSpinner />
+      </div>
+      <ul v-else-if="hasCoaches">
         <CoachItem
           v-for="coach in filteredCoaches"
           :key="coach.id"
@@ -36,6 +39,7 @@ export default {
 
   data() {
     return {
+      isLoading: false,
       activeFilters: {
         frontend: true,
         backend: true,
@@ -56,7 +60,7 @@ export default {
     },
 
     hasCoaches() {
-      return this.$store.getters['coaches/hasCoaches'];
+      return !this.isLoading && this.$store.getters['coaches/hasCoaches'];
     },
 
     isCoach() {
@@ -69,8 +73,12 @@ export default {
       this.activeFilters = updatedFilters;
     },
 
-    loadCoaches() {
-      this.$store.dispatch('coaches/loadCoaches');
+    async loadCoaches() {
+      this.isLoading = true;
+
+      await this.$store.dispatch('coaches/loadCoaches');
+
+      this.isLoading = false;
     },
   },
 
