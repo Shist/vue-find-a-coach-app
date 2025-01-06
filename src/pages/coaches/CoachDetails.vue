60dc6eaa87f12843bb14431234fdd4aa7e68ec49
Shist <shist.pupust@mail.ru>
2025-01-06 11:26:54 +0100

    Implemented Coach Details page with its layout and logic

--- src/pages/coaches/CoachDetails.vue
+++ src/pages/coaches/CoachDetails.vue
@@ -1,5 +1,82 @@
 <template>
-  <h1>CoachDetails</h1>
-  <router-view></router-view>
-  <router-link to="/coaches/example/contact">Contact</router-link>
+  <div v-if="selectedCoach">
+    <section>
+      <BaseCard>
+        <h2>{{ fullName }}</h2>
+        <h3>${{ rate }}/hour</h3>
+      </BaseCard>
+    </section>
+    <section>
+      <BaseCard>
+        <header>
+          <h2>Interested? Reach out now!</h2>
+          <BaseButton link :to="contactLink">Contact</BaseButton>
+        </header>
+        <router-view></router-view>
+      </BaseCard>
+    </section>
+    <section>
+      <BaseCard>
+        <BaseBadge
+          v-for="area in areas"
+          :key="area"
+          :type="area"
+          :title="area"
+        ></BaseBadge>
+        <p>{{ description }}</p>
+      </BaseCard>
+    </section>
+  </div>
+  <h2 v-else class="no-coach-headline">No coach with such ID found...</h2>
 </template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      selectedCoach: null,
+    };
+  },
+
+  computed: {
+    fullName() {
+      return `${this.selectedCoach.firstName} ${this.selectedCoach.lastName}`;
+    },
+
+    areas() {
+      return this.selectedCoach.areas;
+    },
+
+    rate() {
+      return this.selectedCoach.hourlyRate;
+    },
+
+    description() {
+      return this.selectedCoach.description;
+    },
+
+    contactLink() {
+      return `${this.$route.path}/${this.id}/contact`;
+    },
+  },
+
+  created() {
+    this.selectedCoach = this.$store.getters['coaches/coaches'].find(
+      (coach) => coach.id === this.id
+    );
+  },
+};
+</script>
+
+<style scoped>
+.no-coach-headline {
+  text-align: center;
+}
+</style>
