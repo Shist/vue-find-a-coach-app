b6ab63f40a8b9ec5b92e2302d73ca5d5ec115f07
Shist <shist.pupust@mail.ru>
2025-01-06 12:17:59 +0100

    Prepared coach registration form, with its layout and styles

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1>CoachRegistration</h1>
+  <section>
+    <BaseCard>
+      <h2>Register as a coach now!</h2>
+      <CoachForm />
+    </BaseCard>
+  </section>
 </template>
+
+<script>
+import CoachForm from '@/components/coaches/CoachForm.vue';
+
+export default {
+  components: {
+    CoachForm,
+  },
+};
+</script>
